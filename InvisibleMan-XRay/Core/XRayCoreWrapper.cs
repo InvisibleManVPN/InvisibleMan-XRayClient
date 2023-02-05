35eaaee1cce81a05bb18217a5e8c531d3476bb94
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-02-06 00:42:46 +0330

    Split C# wrapper methods

--- InvisibleMan-XRay/Core/XRayCoreWrapper.cs
+++ InvisibleMan-XRay/Core/XRayCoreWrapper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace InvisibleManXRay.Core
@@ -6,7 +7,36 @@ namespace InvisibleManXRay.Core
 
     public class XRayCoreWrapper
     {
-        [DllImport(Path.XRAY_CORE_DLL, EntryPoint = "Run")]
-        public static extern void Run(string path);
+        public static string GetConfigFormat(string path)
+        {
+            return Marshal.PtrToStringAnsi(GetConfigFormat(path));
+
+            [DllImport(Path.XRAY_CORE_DLL, EntryPoint = "GetConfigFormat")]
+            static extern IntPtr GetConfigFormat(string path);
+        }
+
+        public static bool IsFileExists(string path)
+        {
+            return IsFileExists(path);
+
+            [DllImport(Path.XRAY_CORE_DLL, EntryPoint = "IsFileExists")]
+            static extern bool IsFileExists(string path);
+        }
+
+        public static string LoadConfig(string fileFormat, string filePath)
+        {
+            return Marshal.PtrToStringAnsi(LoadConfig(fileFormat, filePath));
+
+            [DllImport(Path.XRAY_CORE_DLL, EntryPoint = "LoadConfig")]
+            static extern IntPtr LoadConfig(string format, string file);
+        }
+
+        public static void StartServer(string config)
+        {
+            StartServer(config);
+
+            [DllImport(Path.XRAY_CORE_DLL, EntryPoint = "StartServer")]
+            static extern void StartServer(string config);
+        }
     }
 }
