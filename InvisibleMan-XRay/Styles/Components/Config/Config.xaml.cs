2e43a148e2d29e1b06aca0997782ac9e12a63a9e
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-02-18 11:32:53 +0330

    Add config editing feature

--- InvisibleMan-XRay/Styles/Components/Config/Config.xaml.cs
+++ InvisibleMan-XRay/Styles/Components/Config/Config.xaml.cs
@@ -1,9 +1,14 @@
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace InvisibleManXRay.Components
 {
     public partial class Config : UserControl
     {
+        private Models.Config config;
+
         public Config()
         {
             InitializeComponent();
@@ -11,8 +16,38 @@ namespace InvisibleManXRay.Components
 
         public void Setup(Models.Config config)
         {
+            this.config = config;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
             textConfigName.Content = config.Name;
             textUpdateTime.Content = config.UpdateTime;
+        }
+
+        private void OnEditButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(config.Path))
+            {
+                MessageBox.Show(
+                    Values.Message.FILE_DOESNT_EXISTS,
+                    Values.Caption.ERROR,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+                return;
+            }
+
+            OpenFileInTextEditor();
+
+            void OpenFileInTextEditor()
+            {
+                Process fileOpenProcess = new Process();
+                fileOpenProcess.StartInfo.FileName = "notepad";
+                fileOpenProcess.StartInfo.Arguments = $"\"{config.Path}\"";
+                fileOpenProcess.Start();
+            }
         }
     }
 }
--- InvisibleMan-XRay/Values/Message.cs
+++ InvisibleMan-XRay/Values/Message.cs
@@ -5,5 +5,6 @@ namespace InvisibleManXRay.Values
         public const string NO_CONFIGS_FOUND = "No configurations were found.\nPlease add a new server configuration.";
         public const string INVALID_CONFIG = "Invalid configuration.\nCheck the server configuration file.";
         public const string NO_FILES_SELECTED = "No files were selected.\nAdd a server configuration file.";
+        public const string FILE_DOESNT_EXISTS = "This file doesn't exists.\nMaybe it has been deleted.";
     }
 }
