2fa45488dcf1b6235ff82f5e6599b73d94c01aac
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-02-23 22:22:04 +0330

    Add overrideInbound function

--- XRay-Wrapper/main.go
+++ XRay-Wrapper/main.go
@@ -11,9 +11,11 @@ func main() {
 	fmt.Println("created by: invisiblemanvpn")
 	fmt.Println("https://github.com/invisiblemanvpn")
 
-	// for testing, you can use xray.RunTest(path) function.
-	// uncomment these lines and replace the "path" variable with your config path.
+	// for testing, you can use xray.RunTest(path, port) function.
+	// uncomment these lines and replace the "path" variable with your config path,
+	// and replace the "port" variable with the port that you want to listen to it.
 
 	// path := "for example: C:/config.json"
-	// xray.RunTest(path)
+	// port := "for example: 10801"
+	// xray.RunTest(path, port)
 }
