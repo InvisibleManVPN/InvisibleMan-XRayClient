e65605db2181fa5b381d51f0ea22cb6be2628c5d
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-03-16 01:55:55 +0330

    Change root directory

--- InvisibleMan-XRay/Values/Directory.cs
+++ InvisibleMan-XRay/Values/Directory.cs
@@ -2,7 +2,7 @@ namespace InvisibleManXRay.Values
 {
     internal static class Directory
     {
-        public const string ROOT = "./InvisibleMan-XRay";
+        public const string ROOT = ".";
         public const string LIBRARIES = $"{ROOT}/Libraries";
         public const string CONFIGS = $"{ROOT}/Configs";
         public const string ASSETS = $"{ROOT}/Assets";
