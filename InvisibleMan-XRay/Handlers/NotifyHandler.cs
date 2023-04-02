a1f7d1feabced3f1474228dcd0104b5f5597fc00
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-04-02 14:59:01 +0330

    Add mode sub menu UI

--- InvisibleMan-XRay/Handlers/NotifyHandler.cs
+++ InvisibleMan-XRay/Handlers/NotifyHandler.cs
@@ -62,17 +62,61 @@ namespace InvisibleManXRay.Handlers
             ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
             
             AddMenuItem("Open Invisible Man XRay", onOpenClick);
+            AddMenuItem("Mode", delegate { }, new ToolStripMenuItem[] {
+                CreateItem("Proxy", delegate { }, true, true),
+                CreateItem("TUN", delegate { }, true, false)
+            });
             AddMenuItem("Check for updates", onUpdateClick);
             AddMenuItem("About", onAboutClick);
             AddMenuItem("Close", onCloseClick);
 
             notifyIcon.ContextMenuStrip = contextMenuStrip;
 
-            void AddMenuItem(string text, Action onClick)
+            void AddMenuItem(string text, Action onClick, ToolStripMenuItem[] children = default)
             {
-                ToolStripMenuItem item = new ToolStripMenuItem() { Text = text };
-                item.Click += (sender, e) => { onClick.Invoke(); };
+                ToolStripMenuItem item = CreateItem(text, onClick);
+
+                if (children != null)
+                    foreach(ToolStripMenuItem child in children)
+                        item.DropDownItems.Add(child);
+                
                 contextMenuStrip.Items.Add(item);
+            }
+
+            ToolStripMenuItem CreateItem(
+                string text, 
+                Action onClick, 
+                bool isToggle = default,
+                bool isChecked = default
+            )
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem() { Text = text, Checked = isChecked };
+                item.Click += (sender, e) => { 
+                    HandleToggleClick();
+                    onClick.Invoke(); 
+                };
+                
+                return item;
+
+                void HandleToggleClick()
+                {
+                    if (!isToggle)
+                        return;
+
+                    UncheckAllItems();
+                    CheckItem(item);
+                }
+
+                void UncheckAllItems()
+                {
+                    foreach(ToolStripMenuItem item in item.GetCurrentParent().Items)
+                        item.Checked = false;
+                }
+
+                void CheckItem(ToolStripMenuItem item)
+                {
+                    item.Checked = true;
+                }
             }
         }
     }
