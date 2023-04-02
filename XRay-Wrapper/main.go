5e28de6834eb357b931714472b5e5184f6b4a538
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-04-02 16:05:11 +0330

    Add overrideInboundToSocks() function

--- XRay-Wrapper/main.go
+++ XRay-Wrapper/main.go
@@ -11,11 +11,13 @@ func main() {
 	fmt.Println("created by: invisiblemanvpn")
 	fmt.Println("https://github.com/invisiblemanvpn")
 
-	// for testing, you can use xray.RunTest(path, port) function.
+	// for testing, you can use xray.RunTest(path, port, isSocks) function.
 	// uncomment these lines and replace the "path" variable with your config path,
 	// and replace the "port" variable with the port that you want to listen to it.
+	// If you want to run it in socks mode, set "isSocks" to true, else false.
 
 	// path := "for example: C:/config.json"
 	// port := "for example: 10801"
-	// xray.RunTest(path, port)
+	// isSocks := "for example: true"
+	// xray.RunTest(path, port, isSocks)
 }
