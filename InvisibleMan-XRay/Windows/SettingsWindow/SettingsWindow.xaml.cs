83d76d249bbf2be493b89449e94dd567f8d6d461
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-05-28 22:13:57 +0330

    Add clicking functionality for all tabs in the settings window

--- InvisibleMan-XRay/Windows/SettingsWindow/SettingsWindow.xaml.cs
+++ InvisibleMan-XRay/Windows/SettingsWindow/SettingsWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Controls;
 
 namespace InvisibleManXRay
 {
@@ -7,6 +8,84 @@ namespace InvisibleManXRay
         public SettingsWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnBasicTabClick(object sender, RoutedEventArgs e)
+        {
+            EnableAllTabs();
+            HideAllPanels();
+
+            SetEnableBasicTabButton(false);
+            SetActiveBasicPanel(true);
+        }
+
+        private void OnPortTabClick(object sender, RoutedEventArgs e)
+        {
+            EnableAllTabs();
+            HideAllPanels();
+
+            SetEnablePortTabButton(false);
+            SetActivePortPanel(true);
+        }
+
+        private void OnTunTabClick(object sender, RoutedEventArgs e)
+        {
+            EnableAllTabs();
+            HideAllPanels();
+
+            SetEnableTunTabButton(false);
+            SetActiveTunPanel(true);
+        }
+
+        private void OnLogTabClick(object sender, RoutedEventArgs e)
+        {
+            EnableAllTabs();
+            HideAllPanels();
+
+            SetEnableLogTabButton(false);
+            SetActiveLogPanel(true);
+        }
+
+        private void SetActiveBasicPanel(bool isActive) => SetActivePanel(panelBasic, isActive);
+
+        private void SetActivePortPanel(bool isActive) => SetActivePanel(panelPort, isActive);
+
+        private void SetActiveTunPanel(bool isActive) => SetActivePanel(panelTun, isActive);
+
+        private void SetActiveLogPanel(bool isActive) => SetActivePanel(panelLog, isActive);
+
+        private void SetActivePanel(Panel panel, bool isActive)
+        {
+            panel.Visibility = isActive ? Visibility.Visible : Visibility.Hidden;
+        }
+        
+        private void HideAllPanels()
+        {
+            SetActiveBasicPanel(false);
+            SetActivePortPanel(false);
+            SetActiveTunPanel(false);
+            SetActiveLogPanel(false);
+        }
+
+        private void SetEnableBasicTabButton(bool isEnabled) => SetEnableButton(buttonBasicTab, isEnabled);
+
+        private void SetEnablePortTabButton(bool isEnabled) => SetEnableButton(buttonPortTab, isEnabled);
+
+        private void SetEnableTunTabButton(bool isEnabled) => SetEnableButton(buttonTunTab, isEnabled);
+
+        private void SetEnableLogTabButton(bool isEnabled) => SetEnableButton(buttonLogTab, isEnabled);
+
+        private void SetEnableButton(Button button, bool isEnabled)
+        {
+            button.IsEnabled = isEnabled;
+        }
+
+        private void EnableAllTabs()
+        {
+            SetEnableBasicTabButton(true);
+            SetEnablePortTabButton(true);
+            SetEnableTunTabButton(true);
+            SetEnableLogTabButton(true);
         }
     }
 }
