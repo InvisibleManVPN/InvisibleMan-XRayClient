36a08e1c49849a754a4e1252d32dfdc848633939
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-07-21 10:29:55 +0330

    Add config hint to the server window

---
+++ InvisibleMan-XRay/Styles/Components/ConfigHint/ConfigHint.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace InvisibleManXRay.Components
+{
+    public partial class ConfigHint : UserControl
+    {
+        public ConfigHint()
+        {
+            InitializeComponent();
+        }
+    }
+}
