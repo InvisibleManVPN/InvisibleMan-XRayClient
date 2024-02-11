ec4f13754807efbbb7d39e2c86ce977dca85ce8b
invisiblemanvpn <invisiblemanvpn@gmail.com>
2024-02-11 18:03:23 +0330

    Add MainTopBar component

---
+++ InvisibleMan-XRay/Styles/Components/MainTopBar/MainTopBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace InvisibleManXRay.Styles.Components
+{
+    public partial class MainTopBar : UserControl
+    {
+        public MainTopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
