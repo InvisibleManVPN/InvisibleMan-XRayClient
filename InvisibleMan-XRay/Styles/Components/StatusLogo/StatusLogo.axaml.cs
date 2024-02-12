becd18788fa8683da86754e94f5d8c3d09015b7f
invisiblemanvpn <invisiblemanvpn@gmail.com>
2024-02-12 12:06:19 +0330

    Add the foundation of StatusLogo component and design MainWindow

---
+++ InvisibleMan-XRay/Styles/Components/StatusLogo/StatusLogo.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace InvisibleManXRay.Styles.Components
+{
+    public partial class StatusLogo : UserControl
+    {
+        public StatusLogo()
+        {
+            InitializeComponent();
+        }
+    }
+}
